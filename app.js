56676c0459704e23c26a6a262e7ec60699a1a4a1
nemes08 <semen.semen.uur@gmail.com>
2025-12-17 12:37:09 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+Pi.init({
+  version: "2.0",
+  sandbox: true
+});
+
+async function startPayment(amount) {
+  try {
+    await Pi.authenticate(["payments"]);
+
+    const payment = await Pi.createPayment({
+      amount: Number(amount),
+      memo: "İlan Yayınlama Bedeli"
+    });
+
+    await Pi.approvePayment(payment.identifier);
+
+    alert("✅ Ödeme başarılı");
+  } catch (e) {
+    alert("❌ Hata: " + e.message);
+  }
+}
