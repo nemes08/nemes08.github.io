6b9720ff3cc84e17b40c22a464450307e93f7133
nemes08 <semen.semen.uur@gmail.com>
2025-12-20 16:48:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("payBtn").addEventListener("click", () => {
-    alert("Buton çalışıyor ✅\n\nBu işlem Pi Browser içinde ödeme başlatır.");
+
+  document.getElementById("payBtn").addEventListener("click", async () => {
+    if (typeof Pi === "undefined") {
+      alert("Bu işlem sadece Pi Browser içinde çalışır.");
+      return;
+    }
+
+    try {
+      await Pi.init({ version: "2.0", sandbox: true });
+
+      const payment = await Pi.createPayment({
+        amount: 0.5,
+        memo: "İlan yayınlama ücreti",
+        metadata: { type: "listing_fee" }
+      });
+
+      alert("Ödeme başlatıldı ✅");
+    } catch (err) {
+      alert("Ödeme iptal edildi veya hata oluştu");
+      console.error(err);
+    }
   });
 
   document.getElementById("requestBtn").addEventListener("click", () => {
     alert("Talep oluşturma yakında aktif.");
   });
+
 });
